d49031361f43295520ddd348ab2d2c67e3a698c5
Dohee Kim <43026521+goodmary1121@users.noreply.github.com>
2021-12-29 19:59:43 +0900

    Update tasks.py

--- caet5/data/tasks.py
+++ caet5/data/tasks.py
@@ -185,7 +185,7 @@ if task_name in FLAGS.mixture_or_task:
                                                            load_config_fn=load_config_acc_fn,
                                                            load_pretrained_fn=load_pretrained_acc_fn,
                                                            batch_size=32,
-                                                           map_location=torch.device('cpu')))
+                                                           map_location=torch.device('tpu')))
 
     if "PPL" in FLAGS.metrics:
         metric_fns_task.append(setup_ppl_parametric_metric(model_filename="gpt2_ppl_yelp.pt",
@@ -194,7 +194,7 @@ if task_name in FLAGS.mixture_or_task:
                                                            load_tokenizer_fn=AutoTokenizer.from_pretrained,
                                                            load_config_fn=AutoConfig.from_pretrained,
                                                            load_pretrained_fn=AutoModelWithLMHead.from_config,
-                                                           map_location=torch.device('cpu'),
+                                                           map_location=torch.device('tpu'),
                                                            batch_size=8,
                                                            block_size=256))
 
