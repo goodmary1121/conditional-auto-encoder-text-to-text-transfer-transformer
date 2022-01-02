43cab4c18b98bbdd6029f1415c6922ef17d90e14
Dohee Kim <43026521+goodmary1121@users.noreply.github.com>
2022-01-02 15:06:11 +0900

    Update metrics_utils.py

--- caet5/evaluation/metrics_utils.py
+++ caet5/evaluation/metrics_utils.py
@@ -1,6 +1,7 @@
 import errno
 import functools
 import os
+import copy
 
 import tensorflow as tf
 import torch
@@ -90,7 +91,7 @@ def load_finetuned_transformer(evaluator_name, finetuned_model_local_path, pretr
     print(finetuned_model_local_path)
     
     try:
-        pretrained_model.load_state_dict(torch.load(finetuned_model_local_path, map_location=map_location))
+        pretrained_model.load_state_dict(copy.deepcopy(torch.load(finetuned_model_local_path, map_location=map_location)))
         # pretrained_model becomes finetuned_model
         pretrained_model = xm.send_cpu_data_to_device(pretrained_model, device)
         # pretrained_model.to(device)
