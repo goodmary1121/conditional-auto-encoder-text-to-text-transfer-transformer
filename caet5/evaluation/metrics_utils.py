8117c9752c2efd5c41f40d2e2072b9b4dd287765
Dohee Kim <43026521+goodmary1121@users.noreply.github.com>
2022-01-02 13:06:03 +0900

    Update metrics_utils.py

--- caet5/evaluation/metrics_utils.py
+++ caet5/evaluation/metrics_utils.py
@@ -90,10 +90,10 @@ def load_finetuned_transformer(evaluator_name, finetuned_model_local_path, pretr
     print(finetuned_model_local_path)
     
     try:
-        pretrained_model.load_state_dict(torch.load(finetuned_model_local_path, map_location="cuda"))
+        pretrained_model.load_state_dict(torch.load(finetuned_model_local_path, map_location=map_location))
         # pretrained_model becomes finetuned_model
-        # pretrained_model = xm.send_cpu_data_to_device(pretrained_model, device)
-        pretrained_model.to(device)
+        pretrained_model = xm.send_cpu_data_to_device(pretrained_model, device)
+        # pretrained_model.to(device)
     except:
         raise RuntimeError('Error(s) in loading state_dict for %s.' % evaluator_name)
 
